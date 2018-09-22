e91949dd7ce1d9c822d4d20e2dab6f70a8a8bd15
Matt Locke <matt@dancingondynamite.com>
2018-09-22 00:44:42 -0600

    convert table to buefy with sortable options

--- src/views/Fantasy.vue
+++ src/views/Fantasy.vue
@@ -3,7 +3,35 @@
     .wrap
       .box
         h1 Fantasy
-        table.table.is-fullwidth
+        b-table(
+          :data="players"
+          :paginated="true"
+          :per-page="50"
+          :current-page.sync="currentPage"
+          default-sort="name"
+        )
+          template(slot-scope="props")
+            b-table-column(
+              field="name"
+              label="Name"
+              sortable
+            ) {{ props.row.name }}
+            b-table-column(
+              field="attributes.player_number"
+              label="Number"
+              sortable
+            ) {{ props.row.attributes.player_number }}
+            b-table-column(
+              field="attributes.role"
+              label="Role"
+              sortable
+            ) {{ props.row.attributes.role }}
+            b-table-column(
+              field="team"
+              label="Team"
+              sortable
+            ) {{ props.row.team }}
+        //- table.table.is-fullwidth
           thead
             tr
               th Name
@@ -25,7 +53,8 @@ export default {
   name: 'fantasy',
   data () {
     return {
-      players: []
+      players: [],
+      currentPage: 1
     }
   },
   mounted () {
