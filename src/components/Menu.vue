a4bb540e3b05f14833feff29ea841f240c392d88
Matt Locke <matt@dancingondynamite.com>
2018-09-22 00:44:24 -0600

    update menu layout

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,23 +2,25 @@
   .menu
     .wrap
       a.is-pulled-right(@click="showMenu = !showMenu") Menu
-    .wrap(v-if="showMenu")
+    .wrap(:class="{opened: showMenu, closed: !showMenu}")
       .main-menu
         ul
-          li
-            router-link(to="/home") News
-          li
-            router-link(to="/fantasy") Fantasy
-          li
-            router-link(to="/leagueFantasy") Fantasy League
-          li
-            router-link(to="/leaguePickem") Pickem Leagues
-          li
-            router-link(to="/picks") My Picks
-          li
-            router-link(to="/profile") My Profile
-          li
-            a(@click="logOut()") Log Out
+          li(@click="showMenu = false")
+            router-link.ow-font(to="/home") News
+          li(@click="showMenu = false")
+            router-link.ow-font(to="/fantasy") Fantasy
+          li(@click="showMenu = false")
+            router-link.ow-font(to="/leagueFantasy") Fantasy League
+          li(@click="showMenu = false")
+            router-link.ow-font(to="/leaguePickem") Pickem Leagues
+          li(@click="showMenu = false")
+            router-link.ow-font(to="/picks") My Picks
+          li(@click="showMenu = false")
+            router-link.ow-font(to="/profile") My Profile
+          li(@click="showMenu = false")
+            router-link.ow-font(to="/createLeague") Create League
+          li(@click="showMenu = false")
+            a.ow-font(@click="logOut()") Log Out
 </template>
 
 <script>
@@ -43,9 +45,49 @@ export default {
 </script>
 
 <style lang="scss">
+  .menu {
+    position: absolute;
+    z-index: 900;
+    top: 0;
+    right: 0;
+    height: 100vh;
+    background-color: rgba(0,0,0,0.8);
+  }
   .main-menu {
     width: 25vw;
-    padding: 1rem;
+    padding: 1rem 1rem 1rem 2vw;
     background-color: rgba(0,0,0,0.3);
+    margin-top: 2rem;
+    ul {
+      li {
+        a {
+          display: block;
+          padding: .25rem .5rem;
+          color: #fff;
+          font-size: 1.4rem;
+          &:hover {
+            background-color: rgba(255,255,255,0.3);
+          }
+        }
+      }
+    }
+  }
+  .opened, .openedParent {
+    animation-name: opening;
+    width: 15vw;
+    animation-duration: .5s;
+  }
+  .closed, .closedParent {
+    animation-name: closing;
+    animation-duration: .5s;
+    width: 2vw;
+  }
+  @keyframes opening {
+    from { width: 2vw; }
+    to { width: 15vw; }
+  }
+  @keyframes closing {
+    from { width: 15vw; }
+    to { width: 2vw; }
   }
 </style>
