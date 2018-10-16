fc1b7cf7a47f2c2b423885cd0477bcfc32240ed7
MattLocke <matt@dancingondynamite.com>
2018-10-15 21:00:32 -0600

    team slot component for fantasy leagues

--- src/views/Fantasy.vue
+++ src/views/Fantasy.vue
@@ -1,61 +1,58 @@
 <template lang="pug">
   .fantasy
-    .columns
-        .column.is-one-quarter
-          .wrap
-            p Fantasy
-        .column
-          .wrap
-            .box
-              h1 Fantasy
-              b-table(
-                :data="players"
-                :paginated="true"
-                :per-page="50"
-                :current-page.sync="currentPage"
-                default-sort="name"
-              )
-                template(slot-scope="props")
-                  b-table-column(
-                    field="name"
-                    label="Name"
-                    sortable
-                  ) {{ props.row.name }}
-                  b-table-column(
-                    field="attributes.player_number"
-                    label="Number"
-                    sortable
-                  ) {{ props.row.attributes.player_number }}
-                  b-table-column(
-                    field="attributes.role"
-                    label="Role"
-                    sortable
-                  ) {{ props.row.attributes.role }}
-                  b-table-column(
-                    field="team"
-                    label="Team"
-                    sortable
-                  ) {{ props.row.team }}
-              //- table.table.is-fullwidth
-                thead
-                  tr
-                    th Name
-                    th Number
-                    th Role
-                    th Team
-                tbody
-                  tr(v-for="player in players" :key="player.name")
-                    td {{ player.name }}
-                    td {{ player.attributes.player_number }}
-                    td {{ player.attributes.role }}
-                    td {{ player.team }}
+    .wrap
+      .box
+        h1 Fantasy
+        hr
+        .columns.is-desktop
+          .column
+            team-slot(roleType="Damage")
+          .column
+            team-slot(roleType="Tank")
+          .column
+            team-slot(roleType="Support")
+          .column
+            team-slot(roleType="Flex")
+        //- b-table(
+          :data="players"
+          :paginated="true"
+          :per-page="50"
+          :current-page.sync="currentPage"
+          default-sort="name"
+          )
+          template(slot-scope="props")
+            b-table-column(
+              field="name"
+              label="Name"
+              sortable
+            ) {{ props.row.name }}
+            b-table-column(
+              field="attributes.player_number"
+              label="Number"
+              sortable
+            ) {{ props.row.attributes.player_number }}
+            b-table-column(
+              field="attributes.role"
+              label="Role"
+              sortable
+            ) {{ props.row.attributes.role }}
+            b-table-column(
+              field="team"
+              label="Team"
+              sortable
+            ) {{ props.row.team }}
 </template>
 
 <script>
 import playerService from '@/services/players'
 
+import TeamSlot from '@/views/fantasy/TeamSlot'
+
 export default {
   name: 'fantasy',
+  components: {
+    TeamSlot
+  },
   data () {
     return {
       players: [],
---
+++ src/views/fantasy/TeamSlot.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .team-slot
+    .mockup
+      .columns
+        .column
+          h1 {{ roleType }}
+        .column.is-narrow
+          a.button.is-primary.is-small View Heroes
+        .column.is-narrow
+          a.button.is-primary.is-small View Players
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      teamName: 'Shock',
+      viewHeroes: false,
+      viewPlayers: false
+    }
+  },
+  props: {
+    roleType: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  a.button {
+    margin-top: 10px;
+  }
+</style>
