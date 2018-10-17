e40fd752f337c068d126920126ac3de6e28c9d86
MattLocke <matt@dancingondynamite.com>
2018-10-17 01:24:25 -0600

    Added base for picture uploads for leagues

---
+++ src/services/image.js
@@ -0,0 +1,57 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import { fireInit } from '@/fireLogin'
+import logger from '@/services/logger'
+
+fireInit()
+
+export default {
+  getTeamLogo (logo) {
+    var pathReference = firebase.storage().ref(`images/team/${logo}`)
+
+    return pathReference.getDownloadURL()
+      .then(url => url)
+      .catch((error) => {
+        logger.errorIt(error)
+        return false
+      })
+  },
+  getLocalTeamLogo (logo) {
+    return `/static/img/team-logos/${logo}`
+  },
+  getCompetitionLogo (logo) {
+    var pathReference = firebase.storage().ref(`images/competitions/${logo}`)
+
+    return pathReference.getDownloadURL()
+      .then(url => url)
+      .catch((error) => {
+        logger.errorIt(error)
+        return false
+      })
+  },
+  getImage (folder, imageName) {
+    var pathReference = firebase.storage().ref(`images/${folder}/${imageName}`)
+
+    return pathReference.getDownloadURL()
+      .then(url => url)
+      .catch((error) => {
+        // Return a default one we have for teams - not made yet!
+        logger.errorIt(error)
+        return false
+      })
+  },
+  uploadImage (folder, imageName, image) {
+    var uploadPath = `images/${folder}/${imageName}`
+    var imageRef = firebase.storage().ref().child(uploadPath)
+
+    return imageRef.put(image)
+      .then((snapshot) => {
+        logger.logIt('Successfully uploaded the image! ', snapshot)
+        return true
+      })
+      .catch((error) => {
+        logger.errorIt(error)
+        return false
+      })
+  }
+}
--- src/views/createLeague/Pickem.vue
+++ src/views/createLeague/Pickem.vue
@@ -4,14 +4,47 @@
       h1 Pick'em Mode
     section
       h2 League Name
-      b-input(placeholder="league name")
+      b-input(placeholder="league name" v-model="leagueName")
     section
       h2 Password Options
       p Would you like to password protect this league?
-      b-switch
+      b-switch(v-model="passwordProtected")
+      b-input(placeholder="password" v-if="passwordProtected" v-model="password")
     section
       h2 League Image
-      p Please select the image to use
+      p Please select the image to use.  Image should be jpg/png and 256x256.  (This can be changed/selected later)
+      picture-input(ref="pictureInput", width="250", height="250", accept="image/jpeg,image/png", buttonClass="button is-primary is-small", @change="uploadPicture")
     section
-      button.button.is-positive Create League
+      button.button.is-primary Create League
 </template>
+
+<script>
+import PictureInput from 'vue-picture-input'
+
+export default {
+  components: {
+    PictureInput
+  },
+  data () {
+    return {
+      fileExtension: null,
+      leagueName: null,
+      ownerId: this.$store.state.user.fireUserData.uid,
+      passwordProtected: false,
+      leagueImage: null,  
+      password: null
+    }
+  },
+  methods: {
+    uploadPicture () {
+      console.log('New picture selected.')
+      if (this.$refs.pictureInput.file) {
+        this.fileExtension = `.${this.$refs.pictureInput.fileType.substr(this.$refs.pictureInput.fileType.indexOf("/") + 1)}`
+        console.table(this.$refs.pictureInput)
+      } else {
+        console.error('FileReader API not supported.  Boo.')
+      }
+    },
+  }
+}
+</script>
