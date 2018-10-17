f9fa8fb04d9dc2c7d22def33c7bd553c49057cb1
MattLocke <matt@dancingondynamite.com>
2018-10-17 01:24:46 -0600

    clean up create league layout

--- src/views/CreateLeague.vue
+++ src/views/CreateLeague.vue
@@ -3,25 +3,31 @@
     .box(v-if="numLeagues < 4")
       h1 Create League
       hr
-      section(v-if="typeChosen")
-        button.button.is-positive(@click="resetChoice()") Cancel
-      section(v-else)
-        h2 League Type
-        .columns
-          .column.is-half
-            h3 Pick'Em
-            p You and an UNLIMITED number of friends/peers pick who will win each week, and prizes may be awarded by tickets or points.
-            button.button.is-positive(@click="choosePickem()") Choose Pick'Em
-          .column.is-half
-            h3 Traditional Fantasy
-            p You and up to 11 other buddies will form teams in a draft-like fashion, and will duke it out each week.  Points and record will be available for leaderboards.
-            button.button.is-positive(@click="chooseFantasy()") Choose Fantasy
-      pickem(v-if="showPickem" @complete="createLeague()")
-      fantasy(v-if="showFantasy" @complete="createLeague()")
+      .container
+        section(v-if="typeChosen")
+          button.button.is-primary(@click="resetChoice()") Cancel
+        section(v-else)
+          h2 League Type
+          hr
+          .columns
+            .column.is-half-desktop
+              .box
+                h3.ow-font Pick'Em
+                p You and an UNLIMITED number of friends/peers pick who will win each week, and prizes may be awarded by tickets or points.
+              button.button.is-primary(@click="choosePickem()") Choose Pick'Em
+            .column.is-half-desktop
+              .box
+                h3.ow-font Traditional Fantasy
+                p You and up to 11 other buddies will form teams in a draft-like fashion, and will duke it out each week.  Points and record will be available for leaderboards.
+              button.button.is-primary(@click="chooseFantasy()") Choose Fantasy
+        pickem(v-if="showPickem" @complete="createLeague()")
+        fantasy(v-if="showFantasy" @complete="createLeague()")
     .box(v-else)
-      p I'm sorry, looks like you're all out of leagues!  Feel free to
-        a(href="#")  support me through patreon
-        |  to have unlimited leagues!
+      .container
+        section
+          p I'm sorry, looks like you're all out of leagues!  Feel free to
+            a(href="#")  support me through patreon
+            |  to have unlimited leagues!
 </template>
 
 <script>
