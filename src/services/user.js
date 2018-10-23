34d48f5ba178c3f58ff863e9d12b684b864ede49
MattLocke <matt@dancingondynamite.com>
2018-10-23 01:37:44 -0600

    clean up of user service

--- src/services/user.js
+++ src/services/user.js
@@ -9,13 +9,13 @@ var db = firebase.firestore()
 export default {
   createProfile (data) {
     return db.collection('users')
-    .doc(data.id)
-    .set(data)
-    .then(userRef => userRef)
-    .catch((error) => {
-      logger.errorIt(`Error adding user: ${error}`)
-      return false
-    })
+      .doc(data.id)
+      .set(data)
+      .then(userRef => userRef)
+      .catch((error) => {
+        logger.errorIt(`Error adding user: ${error}`)
+        return false
+      })
   },
   getProfile (userId) {
     logger.logIt('Getting user with id of: ' + userId)
@@ -30,22 +30,21 @@ export default {
   setLastActive (userId) {
     var timestamp = Math.round((parseInt(new Date().getTime())) / 1000)
     return db.collection('users').doc(userId)
-    .update({lastActive: timestamp})
-    .then(() => true)
+      .update({ lastActive: timestamp })
+      .then(() => true)
   },
   updateProfile (userData, userId) {
     return db.collection('users').doc(userId)
-    .update(userData)
-    .then(() => true)
-    .catch((error) => {
-      logger.errorIt(error)
-      return false
-    })
+      .update(userData)
+      .then(() => true)
+      .catch((error) => {
+        logger.errorIt(error)
+        return false
+      })
   },
   getAllUsers () {
-    var theList = []
     return db.collection('users')
-    .get()
-    .then((users) => users.docs.map(user => user.data()))
+      .get()
+      .then((users) => users.docs.map(user => user.data()))
   }
 }
