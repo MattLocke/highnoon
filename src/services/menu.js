34c42cd395bb32abfbf0d6893b90b544f10c3ec0
MattLocke <matt@dancingondynamite.com>
2018-10-31 02:22:57 -0600

    Create menu service to centralize links

---
+++ src/services/menu.js
@@ -0,0 +1,15 @@
+export default {
+  getMainMenuItems () {
+    return [
+      { name: 'News', where: '/home' }
+      // { name: 'Fantasy', where: '/fantasy' },
+      // { name: 'Fantasy League', where: '/leagueFantasy' },
+      // { name: 'Pickem Leagues', where: '/leaguePickem' },
+      // { name: 'My Picks', where: '/picks' },
+      // { name: 'My Profile', where: '/profile' },
+      // { name: 'Create League', where: '/createLeague' },
+      // { name: 'Messages', where: '/messages' },
+      // { name: 'Staff', where: '/staff' }
+    ]
+  }
+}
