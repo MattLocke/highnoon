ee228f9652c073b4b3fa7b52e447a76429c4be97
MattLocke <matt@dancingondynamite.com>
2018-10-31 00:49:47 -0600

    Add animation to left menu

--- src/components/LeftBar.vue
+++ src/components/LeftBar.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
   .left-bar.column(v-bind:class="getClass()")
-    burger.is-pulled-right(v-model="isCollapsed")
+    .columns
+      .column
+      .column.is-narrow
+        burger(v-model="isCollapsed" :starts-open="true")
     slot(v-if="!isCollapsed")
 </template>
 
@@ -18,3 +21,27 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .left-bar {
+    padding-top: 1.5rem;
+  }
+  .is-one-quarter, .openedParent {
+    animation-name: opening;
+    width: 25vw;
+    animation-duration: .5s;
+  }
+  .is-narrow, .closedParent {
+    animation-name: closing;
+    animation-duration: .5s;
+    width: 2vw;
+  }
+  @keyframes opening {
+    from { width: 2vw; }
+    to { width: 25vw; }
+  }
+  @keyframes closing {
+    from { width: 15vw; }
+    to { width: 2vw; }
+  }
+</style>
