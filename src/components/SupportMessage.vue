75c6886e11267d091cfa2fcbeaea3883678e4273
MattLocke <matt@dancingondynamite.com>
2018-11-01 02:14:42 -0600

    Add patreon/paypal links to menu

--- src/components/SupportMessage.vue
+++ src/components/SupportMessage.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .support-message
     .box
-      p This site is currently in development for version 2.0  If you have any questions or issues, please
+      p This site is currently in development for version 2.0 and is very much an alpha.  If you have any questions or issues, please
       a(href="https://discord.gg/H5bNcYD")  find us on discord
       | .
       p If you'd like to hook me up with a cup of coffee to keep me going, feel free to do so
