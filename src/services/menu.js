b84c112843acab2793da9eaaf4eacda076e23876
MattLocke <matt@dancingondynamite.com>
2018-11-05 22:27:47 -0700

    make menu react to environment

--- src/services/menu.js
+++ src/services/menu.js
@@ -1,15 +1,56 @@
 export default {
   getMainMenuItems () {
-    return [
-      { name: 'News', where: '/home' }
-      // { name: 'Fantasy', where: '/fantasy' },
-      // { name: 'Fantasy League', where: '/leagueFantasy' },
-      // { name: 'Pickem Leagues', where: '/leaguePickem' },
-      // { name: 'My Picks', where: '/picks' },
-      // { name: 'My Profile', where: '/profile' },
-      // { name: 'Create League', where: '/createLeague' },
-      // { name: 'Messages', where: '/messages' },
-      // { name: 'Staff', where: '/staff' }
+    const prodMenu = [
+      {
+        name: 'News',
+        where: '/home'
+      }
     ]
+    const devMenu = [
+      {
+        name: 'News',
+        where: '/home'
+      },
+      {
+        name: 'Fantasy',
+        where: '/fantasy',
+        requiresAuth: true
+      },
+      {
+        name: 'Fantasy League',
+        where: '/leagueFantasy',
+        requiresAuth: true
+      },
+      {
+        name: 'Pickem Leagues',
+        where: '/leaguePickem',
+        requiresAuth: true
+      },
+      {
+        name: 'My Picks',
+        where: '/picks',
+        requiresAuth: true
+      },
+      {
+        name: 'My Profile',
+        where: '/profile',
+        requiresAuth: true
+      },
+      {
+        name: 'Create League',
+        where: '/createLeague',
+        requiresAuth: true
+      },
+      {
+        name: 'Messages',
+        where: '/messages',
+        requiresAuth: true
+      },
+      {
+        name: 'Staff',
+        where: '/staff'
+      }
+    ]
+    return process.env.VUE_APP_LOADPROD ? prodMenu : devMenu
   }
 }
