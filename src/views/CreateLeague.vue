af0df1f55a0f956ed2868144fa6aca1812103844
MattLocke <matt@dancingondynamite.com>
2018-11-28 01:13:23 -0700

    clean up create league component

--- src/views/CreateLeague.vue
+++ src/views/CreateLeague.vue
@@ -1,27 +1,24 @@
 <template lang="pug">
   .create-league
-    .box(v-if="numLeagues < 4")
-      h1 Create League
-      hr
-      .container
-        section(v-if="typeChosen")
-          button.button.is-primary(@click="resetChoice()") Cancel
-        section(v-else)
-          h2 League Type
-          hr
-          .columns
-            .column.is-half-desktop
-              .box
-                h3 Pick'Em
-                p You and an UNLIMITED number of friends/peers pick who will win each week, and prizes may be awarded by tickets or points.
-              button.button.is-primary(@click="choosePickem()") Choose Pick'Em
-            .column.is-half-desktop
-              .box
-                h3 Traditional Fantasy
-                p You and up to 11 other buddies will form teams in a draft-like fashion, and will duke it out each week.  Points and record will be available for leaderboards.
-              button.button.is-primary(@click="chooseFantasy()") Choose Fantasy
-        pickem(v-if="showPickem" @complete="createLeague()")
-        fantasy(v-if="showFantasy" @complete="createLeague()")
+    .container(v-if="numLeagues < 4")
+      section(v-if="!typeChosen")
+        h1 Create League
+        p Choose a league type below to get started.
+      .columns(v-if="!typeChosen")
+        .column.is-half-desktop
+          section
+            h2 Pick'Em
+            p You and an UNLIMITED number of friends/peers pick who will win each week, and prizes may be awarded by tickets or points.
+            hr
+            button.button.is-primary(@click="choosePickem()") Choose Pick'Em
+        .column.is-half-desktop
+          section
+            h2 Traditional Fantasy
+            p You and up to 11 other buddies will form teams in a draft-like fashion, and will duke it out each week.  Points and record will be available for leaderboards.
+            hr
+            button.button.is-primary(@click="chooseFantasy()") Choose Fantasy
+      pickem(v-if="showPickem" @complete="createLeague()" @cancel="resetChoice")
+      fantasy(v-if="showFantasy" @complete="createLeague()" @cancel="resetChoice")
     .box(v-else)
       .container
         section
