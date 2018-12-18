0a6d3c7465e58f0ea34430f9f650f54880a5af69
MattLocke <matt@dancingondynamite.com>
2018-12-17 21:21:17 -0700

    Remove old code, add color to player profile pictures

--- src/views/picks/TeamProfile.vue
+++ src/views/picks/TeamProfile.vue
@@ -1,26 +1,9 @@
 <template lang="pug">
   .team-profile
-    //- h2 Accounts
-    //- ul
-      li(v-for="account in team.accounts")
-        a(:href="account.value") {{ account.accountType }}
     h2 Players
     .columns.is-multiline
       .column.is-one-third(v-for="player in players")
-        player-card(:player="player.player" :showRemove="false")
-    //- table.table.is-fullwidth
-      thead
-        tr
-          th Handle
-          th Role
-          th Most Played Hero
-      tbody(v-if="players")
-        tr(v-for="playerObj in players")
-          td
-            router-link(:to="playerUrl(playerObj.player)") {{ playerObj.player.name }}
-          td {{ playerObj.player.attributes.role }}
-          td.is-proper(v-if="playerObj.player.attributes.heroes") {{ playerObj.player.attributes.heroes[0] }}
-          td(v-else) N/A
+        player-card(:player="player.player" :showRemove="false" :primaryColor="team.primaryColor == '000000' ? team.secondaryColor : team.primaryColor")
 </template>
 
 <script>
