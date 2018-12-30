843d99f3bc283f4b637c169e18a73b264ad71ac5
MattLocke <matt@dancingondynamite.com>
2018-12-29 23:34:54 -0700

    Enable live draft mode

--- src/views/Draft.vue
+++ src/views/Draft.vue
@@ -1,45 +1,46 @@
 <template lang="pug">
   .draft
-    .columns.is-marginless.is-desktop
+    .columns.is-marginless
       left-bar(:showClose="false")
         h2 Your Roster - {{ roster.length }} of 9
         section
           .roster-section
-            .columns.is-multiline
-              .column.is-one-third(v-for="player in roster")
+            .columns.is-multiline.is-mobile
+              .column.is-one-third-desktop.is-half-mobile(v-for="player in roster")
                 player-card(:player="player" :showRemove="false" :primaryColor="getColor(player)" :score="player.stats.fantasyScore || 0")
-              .column.is-one-third(v-for="placeholder in placeholders")
+              .column.is-one-third-desktop.is-half-mobile(v-for="placeholder in placeholders")
                 player-card(:showRemove="false")
         section
           h3 Remaining Requirements
-          .columns
+          .columns.is-mobile
             .column.has-text-centered
               .big-number
                 img(src="images/roles/offense-white.svg" alt="offense")
-                | {{ remainingOffense }}
+                | {{ remaining.offense }}
             .column.has-text-centered
               .big-number
                 img(src="images/roles/support-white.svg" alt="support")
-                | {{ remainingSupport }}
+                | {{ remaining.support }}
             .column.has-text-centered
               .big-number
                 img(src="images/roles/tank-white.svg" alt="tank")
-                | {{ remainingTank }}
+                | {{ remaining.tank }}
             .column.has-text-centered
               .big-number
                 img(src="images/roles/flex-white.svg" alt="flex")
-                | {{ remainingFlex }}
+                | {{ remaining.flex }}
         h3 Draft Order
-        .left-bar-item(v-for="(user, index) in users" :class="{'active-item': user.isActive}")
-          .columns
+        .left-bar-item(v-for="(user, index) in users" :class="{'active-item': index === draft.activeDrafter}")
+          .columns.is-mobile
             .column.is-narrow
               span {{ index + 1 }}
             .column
-              span {{ user.userDisplayName }}
+              span {{ user.displayName }}
       .column
         .container
           h2 Draft For Some Awesome League
-          .columns.is-desktop
+          //- Choose your player
+          .columns.is-desktop(v-if="myTurn")
             .column(v-if="roster.length < 9")
               section
                 h3 Choose Your Player
@@ -61,26 +62,33 @@
                 b-field(label="Filter Players")
                   b-input(type="text" v-model="filterText")
               section
-                player-line(:player="player" :key="`${Math.random()}${player.id}`" v-for="player in filteredPlayers" @add-player="addToRoster($event)" :canSelect="canSelect(player)" :roster="roster")
+                player-line(:player="player" :key="`${Math.random()}${player.id}`" v-for="player in filteredPlayers" @add-player="addToRoster($event)" :canSelect="canSelect(player)" :roster="selectedPlayers")
             .column(v-else)
               section
                 p Congrats!  Your team is complete!
             .column
               .columns.is-multiline
-                .column.is-one-fifth.draft-user(v-for="(user, index) in users" :class="{'active-item': user.isActive}")
-                  .columns
-                    .column.is-narrow
-                      span {{ index + 1 }}
-                    .column
-                      span {{ user.userDisplayName }}
+                .column.is-narrow.draft-user(v-for="(user, index) in users" :class="{'active-item': index === draft.activeDrafter}")
+                  span {{ index + 1 }}. {{ user.displayName }}
               trash-talk
+          .column(v-else)
+            //- See how the draft is going
+            section
+              h3 Currently Drafting:
+                span.orange  {{ users[draft.activeDrafter].displayName }}
+            .columns.is-multiline.is-mobile
+              .column.is-one-fifth-desktop.is-half-mobile(v-for="(userPicks, index) in picks")
+                h3(:class="{'orange': index == users[draft.activeDrafter].userId}") {{ getUserName(index) }}
+                ul
+                  player-card(v-for="pick in userPicks" :key="pick.id" :player="pick" :showRemove="false" :primaryColor="getColor(pick)" :score="pick.stats.fantasyScore || 0" :hidePhoto="true") {{ pick.name }}
+      draft-drawer(:roster="roster")
 </template>
 
 <script>
 import firebase from 'firebase/app'
 import 'firebase/database'
-import { fireInit } from '@/fireLogin'
 
+import DraftDrawer from '@/views/draft/DraftDrawer'
 import PlayerCard from '@/components/PlayerCard'
 import PlayerLine from '@/views/draft/PlayerLine'
 import TrashTalk from '@/views/draft/TrashTalk'
@@ -88,55 +96,21 @@ import TrashTalk from '@/views/draft/TrashTalk'
 export default {
   name: 'Draft',
   components: {
+    DraftDrawer,
     PlayerCard,
     PlayerLine,
     TrashTalk
   },
   data () {
     return {
+      draft: {
+        activeDrafter: null
+      },
       filterText: '',
       filterTeam: '',
       filterRole: '',
-      users: [
-        {
-          userDisplayName: 'SouldrinK',
-          isActive: true
-        },
-        {
-          userDisplayName: 'Moosetube'
-        },
-        {
-          userDisplayName: 'Morgahl'
-        },
-        {
-          userDisplayName: 'Aretha'
-        },
-        {
-          userDisplayName: 'Plague'
-        },
-        {
-          userDisplayName: 'Rage'
-        },
-        {
-          userDisplayName: 'Slothpaladin'
-        },
-        {
-          userDisplayName: 'Peacecamper'
-        },
-        {
-          userDisplayName: 'Sinyua'
-        },
-        {
-          userDisplayName: 'Sneezle'
-        },
-        {
-          userDisplayName: 'Gig'
-        },
-        {
-          userDisplayName: 'Thawn'
-        }
-      ],
-      roster: [],
+      picks: [],
+      users: [],
       requiredFlex: 0,
       requiredOffense: 2,
       requiredSupport: 2,
@@ -147,11 +121,25 @@ export default {
     players () {
       return this.$store.getters.getPlayers
     },
+    myTurn () {
+      if (this.users.length) return this.users[this.draft.activeDrafter].userId === this.userId
+      return false
+    },
+    roster () {
+      return this.picks[this.userId] || []
+    },
     stats () {
       return this.$store.getters.getStats
     },
     teams () {
       return this.$store.getters.getTeams
+    },
+    selectedPlayers () {
+      let selected = []
+      Object.keys(this.picks).forEach((key) => {
+        selected = [...selected, ...this.picks[key]]
+      })
+      return selected
     },
     placeholders () {
       let remaining = 9 - this.roster.length
@@ -182,24 +170,16 @@ export default {
     tankPlayers () {
       return this.roster.filter(player => player.attributes.role === 'tank')
     },
-    remainingFlex () {
-      const num = this.requiredFlex - this.flexPlayers.length
-      return num > 0 ? num : 0
-    },
-    remainingOffense () {
-      const num = this.requiredOffense - this.offensePlayers.length
-      return num > 0 ? num : 0
-    },
-    remainingSupport () {
-      const num = this.requiredSupport - this.supportPlayers.length
-      return num > 0 ? num : 0
-    },
-    remainingTank () {
-      const num = this.requiredTank - this.tankPlayers.length
-      return num > 0 ? num : 0
+    remaining () {
+      const remaining = {}
+      remaining.flex = this.requiredFlex - this.flexPlayers.length > 0 ? this.requiredFlex - this.flexPlayers.length : 0
+      remaining.offense = this.requiredOffense - this.offensePlayers.length > 0 ? this.requiredOffense - this.offensePlayers.length : 0
+      remaining.tank = this.requiredTank - this.tankPlayers.length > 0 ? this.requiredTank - this.tankPlayers.length : 0
+      remaining.support = this.requiredSupport - this.supportPlayers.length > 0 ? this.requiredSupport - this.supportPlayers.length : 0
+      return remaining
     },
     totalRemaining () {
-      return this.remainingFlex + this.remainingOffense + this.remainingSupport + this.remainingTank
+      return this.remaining.flex + this.remaining.offense + this.remaining.support + this.remaining.tank
     },
     userId () {
       return this.$store.getters.getUserId
@@ -209,6 +189,11 @@ export default {
     }
   },
   watch: {
+    draft: {
+      handler (val) {
+        if (!val || val.status !== 'active') this.$router.push({ path: `/leagues/${this.leagueId}` })
+      }
+    },
     players: {
       immediate: true,
       handler (val) {
@@ -221,7 +206,11 @@ export default {
     userId: {
       immediate: true,
       handler (val) {
-        if (val) this.getRoster()
+        if (val) {
+          this.getPicks()
+          this.getDraft()
+          this.getDraftOrder()
+        }
       }
     },
     teams: {
@@ -233,17 +222,41 @@ export default {
   },
   methods: {
     addToRoster (player) {
+      this.$store.dispatch('setLoading', true)
       const db = firebase.database()
       const tmp = [...this.roster]
       tmp.push(player)
 
       db.ref(`/draftPicks/${this.leagueId}/${this.userId}`)
         .set(tmp)
+        .then(() => {
+          return db.ref(`/draft/${this.leagueId}`)
+            .update({ doneProcessing: false })
+        })
+        .then(() => {
+          this.$store.dispatch('setLoading', false)
+        })
     },
-    getRoster () {
+    getDraft () {
+      if (this.leagueId) {
+        const db = firebase.database()
+        db.ref(`/draft/${this.leagueId}`).on('value', (snapshot) => {
+          this.draft = snapshot.val()
+        })
+      }
+    },
+    getDraftOrder () {
+      if (this.leagueId) {
+        const db = firebase.database()
+        db.ref(`/draftOrder/${this.leagueId}`).on('value', (snapshot) => {
+          this.users = snapshot.val()
+        })
+      }
+    },
+    getPicks () {
       const db = firebase.database()
-      db.ref(`/draftPicks/${this.leagueId}/${this.userId}`).on('value', (snapshot) => {
-        this.roster = snapshot.val() || []
+      db.ref(`/draftPicks/${this.leagueId}`).on('value', (snapshot) => {
+        this.picks = snapshot.val() || []
       })
     },
     getColor (player) {
@@ -255,8 +268,12 @@ export default {
       const match = this.stats.filter(stat => stat.playerId === player.id)
       return match[0] ? match[0].fantasyScore : 0
     },
+    getUserName (id) {
+      return this.users.find(user => user.userId === id).displayName
+    },
     canSelect (player) {
       // if they met all the requirements, they can pick whoever!
+      if (!this.draft.doneProcessing) return false
       if (this.totalRemaining === 0) return true
 
       // if there is only enough for roles that need feeling, strict mode!
@@ -264,16 +281,16 @@ export default {
 
       switch (player.attributes.role) {
         case 'flex': {
-          return strictMode ? !!(this.remainingFlex > 0) : true
+          return strictMode ? !!(this.remaining.flex > 0) : true
         }
         case 'offense': {
-          return strictMode ? !!(this.remainingOffense > 0) : true
+          return strictMode ? !!(this.remaining.offense > 0) : true
         }
         case 'support': {
-          return strictMode ? !!(this.remainingSupport > 0) : true
+          return strictMode ? !!(this.remaining.support > 0) : true
         }
         case 'tank': {
-          return strictMode ? !!(this.remainingTank > 0) : true
+          return strictMode ? !!(this.remaining.tank > 0) : true
         }
         default: return false
       }
@@ -283,8 +300,25 @@ export default {
 </script>
 
 <style lang="scss">
+.draft {
+  margin-bottom: 100px;
+}
 .draft-user {
   background-color: rgba(255,255,255,0.1);
   margin: .25rem;
+  &.active-item {
+    background-color: #f00;
+  }
+}
+.draft-drawer {
+  position: fixed;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.8);
+  width: 100%;
+  .role-image {
+    height: 18px;
+    width: auto;
+    margin-bottom: -3px;
+  }
 }
 </style>
