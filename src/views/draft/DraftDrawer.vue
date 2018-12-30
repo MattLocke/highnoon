c3fbcf87b786b5eb162650d42e0fe615071fa792
MattLocke <matt@dancingondynamite.com>
2018-12-29 23:35:47 -0700

    Draft drawer to show picks at bottom of the screen

---
+++ src/views/draft/DraftDrawer.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .draft-drawer.has-text-centered
+    .columns.is-mobile.is-multiline.is-marginless
+      .column
+      .column.is-narrow
+        h4.orange.ow-font Your Picks - {{ roster.length }} of 9
+      .column.is-narrow(v-for="player in roster")
+        img.role-image(:src="`images/roles/${player.attributes.role || 'flex'}-white.svg`")
+        span.ow-font {{ player.name }}
+      .column
+</template>
+
+<script>
+export default {
+  name: 'draftDrawer',
+  props: {
+    roster: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
