fb4e60b1738ed73178cd5dc7a0ede84d36425642
MattLocke <matt@dancingondynamite.com>
2019-01-06 02:11:53 -0700

    remove click handler from arrows

--- src/components/Arrow.vue
+++ src/components/Arrow.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   .arrow.is-pulled-right
-    svg(viewBox='0 0 50 80', xml:space='preserve' v-if="isLeft" :class="{'clicked': isClicked}" @click="toggle")
+    svg(viewBox='0 0 50 80', xml:space='preserve' v-if="isLeft" :class="{'clicked': isClicked}")
       polyline(fill='none', stroke='#FFFFFF', stroke-width='5', stroke-linecap='round', stroke-linejoin='round', points='\
       45.63,75.8 0.375,38.087 45.63,0.375 ')
-    svg(xmlns='http://www.w3.org/2000/svg', xmlns:xlink='http://www.w3.org/1999/xlink', viewBox='0 0 50 80', xml:space='preserve' v-if="isRight" @click="toggle")
+    svg(xmlns='http://www.w3.org/2000/svg', xmlns:xlink='http://www.w3.org/1999/xlink', viewBox='0 0 50 80', xml:space='preserve' v-if="isRight")
       polyline(fill='none', stroke='#FFFFFF', stroke-width='5', stroke-linecap='round', stroke-linejoin='round', points='\
       0.375,0.375 45.63,38.087 0.375,75.8 ')
 </template>
