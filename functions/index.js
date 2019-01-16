fde126fece77d656dc334932a9e388a066697612
MattLocke <matt@dancingondynamite.com>
2019-01-15 23:13:21 -0700

    auto-picker working for those with a preference list

--- functions/index.js
+++ functions/index.js
@@ -31,6 +31,7 @@ exports.tryAutomatedPick = functions.database.ref('/draft/{leagueId}')
     let selectedPlayer = null
 
     if (after.status === 'active') {
+      console.log('===================================')
       console.log(`We detected activity for league: ${leagueId}.  Checking for preference list.`)
       return admin.database().ref(`/draftOrder/${leagueId}`).once('value')
         .then((users) => {
@@ -42,15 +43,23 @@ exports.tryAutomatedPick = functions.database.ref('/draft/{leagueId}')
           return admin.database().ref(`/draftPreference/${leagueId}/${user.userId}`).once('value')
         })
         .then((preferenceList) => {
-          console.log(`We ${preferenceList ? 'found ' : 'did not find '}a preference list.`)
-          selectedPlayer = preferenceList ? processPreferenceList(preferenceList) : null
+          console.log(`We ${preferenceList.val() ? 'found ' : 'did not find '}a preference list.`)
+          return processPreferenceList(preferenceList.val(), after, leagueId)
+        })
+        .then((thePlayer) => {
+          selectedPlayer = thePlayer
+          // console.log(`We have a selected player value of: ${selectedPlayer.name}`)
           return selectedPlayer ? admin.database().ref(`/draftPicks/${leagueId}/${userId}`).once('value') : null
         })
         .then((userPicks) => {
           if (userPicks) {
-            userPicks.push(selectedPlayer)
-            return admin.database().ref(`/draftPicks/${leagueId}/${userId}`).set(userPicks)
+            var tmpPicks = userPicks.val() || null
+            if (tmpPicks) tmpPicks.push(selectedPlayer)
+            else tmpPicks = [selectedPlayer]
+            console.log(`Setting a pick at : /draftPicks/${leagueId}/${userId} of ${selectedPlayer.name}`)
+            return admin.database().ref(`/draftPicks/${leagueId}/${userId}`).set(tmpPicks)
           }
+          console.log('Not setting anything.  Moving on!')
           return null
         })
     } else {
@@ -64,7 +73,7 @@ function workDraftPick (leagueId, picks) {
 
   return admin.database().ref(`/draft/${leagueId}`).once('value')
     .then((snapshot) => {
-      console.log(`-----Setting the draft for league: ${leagueId}`)
+      // console.log(`-----Setting the draft for league: ${leagueId}`)
       draft = snapshot.val()
       return null
     })
@@ -114,10 +123,10 @@ function workDraftPick (leagueId, picks) {
 
       // If we're maxed on picks, just end the draft
       if (totalPicks >= (draftOrder.length * 9)) {
-        console.log(`------Ending the draft for league: ${leagueId} because total picks (${totalPicks}) was greater than or equal to the max of: ${draftOrder.length * 9}.`)
+        // console.log(`------Ending the draft for league: ${leagueId} because total picks (${totalPicks}) was greater than or equal to the max of: ${draftOrder.length * 9}.`)
         return admin.database().ref(`/draft/${leagueId}/status`).set('completed')
       }
-      console.log(`------Setting the draft position to ${draftPosition} for league: ${leagueId} with a doneProcessing value of: ${localDraft.doneProcessing}`)
+      // console.log(`------Setting the draft position to ${draftPosition} for league: ${leagueId} with a doneProcessing value of: ${localDraft.doneProcessing}`)
       return admin.database().ref(`/draft/${leagueId}`).set(localDraft)
     })
     .catch((error) => {
@@ -126,37 +135,44 @@ function workDraftPick (leagueId, picks) {
 }
 
 function processPreferenceList (preferenceList, league, leagueId) {
-  console.log(`We found a preference list.  Checking for available players in ${leagueId}`)
-  let players = [...league.players]
-  let draftPicks = []
-  let lumpedPicks = []
-  let unclaimedPlayers = []
-  let unclaimedPreference = []
-  // get users to get an id
-  return admin.database().ref(`/draftOrder/${leagueId}`).once('value')
-      .then((users) => {
-        return users.val()[after.activeDrafter]
-      })
-      .then((user) => {
-        useuId = user.userId
-        // get draftPicks
-        return admin.database().ref(`/draftPicks/${leagueId}`).once('value')
-      })
-      .then((thePicks) => {
-        // build draft picks into single array
-        draftPicks = thePicks.val()
-        draftPicks.forEach((picks) => {
-          lumpedPicks = [...lumpedPicks, picks]
+  if (preferenceList) {
+    console.log(`We found a preference list.  Checking for available players in ${leagueId}`)
+    let players = [...league.players]
+    let draftPicks = []
+    let lumpedPicks = []
+    let unclaimedPlayers = []
+    let unclaimedPreference = []
+    // get users to get an id
+    return admin.database().ref(`/draftOrder/${leagueId}`).once('value')
+        .then((users) => {
+          return users.val()[league.activeDrafter]
         })
-        // remove array from preferenceList
-        unclaimedPlayers = _.differenceWith(players, lumpedPicks, _.isEqual)
-        unclaimedPreference = _.differenceWith(preferenceList, unclaimedPlayers, _.isEqual)
-        // if we have a preference list left, take the top player.
-        console.log(`Trying to use an unclaimed preference player. There are ${unclaimedPreference.length} unclaimed players on the preference list.`)
-        if (unclaimedPreference.length) return unclaimedPreference[0]
-        // if the preference list is gone, take the first available player
-        else return unclaimedPlayers[0]
-      }).catch((error) => {
-        console.log(error)
-      })
+        .then((user) => {
+          useuId = user.userId
+          // get draftPicks
+          return admin.database().ref(`/draftPicks/${leagueId}`).once('value')
+        })
+        .then((thePicks) => {
+          // build draft picks into single array
+          var tmp = thePicks.val() ? Object.values(thePicks.val()) : []
+          tmp.forEach((child) => {
+            if (child.length) child.forEach(pick => lumpedPicks.push(pick))
+          })
+
+          if (lumpedPicks) console.log(`We have ${lumpedPicks.length} picks so far.`)
+          // remove array from preferenceList
+          unclaimedPlayers = _.differenceWith(players, lumpedPicks, _.isEqual)
+          console.log(`We have ${players.length} players total.`)
+          unclaimedPreference = unclaimedPlayers.filter(o1 => preferenceList.some(o2 => o1.name === o2.name));
+          // if we have a preference list left, take the top player.
+          if (unclaimedPreference && unclaimedPreference.length) return unclaimedPreference[0]
+          // if the preference list is gone, take the first available player
+          return unclaimedPlayers[0]
+        }).catch((error) => {
+          console.log(error)
+        })
+  } else {
+    console.log('No player preference list found for user.')
+    return null
+  }
 }
