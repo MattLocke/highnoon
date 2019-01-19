6ebfe93370a4350f88e10c012cab50062dba55da
MattLocke <matt@dancingondynamite.com>
2019-01-19 03:07:06 -0700

    Move base league data to state store

--- src/store/leagues.js
+++ src/store/leagues.js
@@ -9,16 +9,24 @@ db.settings({ timestampsInSnapshots: true })
 
 export default {
   state: {
-    leagues: []
+    leagues: [],
+    leagueUsers: [],
+    leagueSchedule: []
   },
   mutations: {
     SET_LEAGUES: (state, payload) => {
       state.leagues = payload
+    },
+    SET_LEAGUE_USERS: (state, payload) => {
+      state.leagueUsers = payload
+    },
+    SET_LEAGUE_SCHEDULE: (state, payload) => {
+      state.leagueSchedule = payload
     }
   },
   actions: {
-    getLeagues: (context, payload) => {
-      if (payload) {
+    getLeagues: ({ state, commit }, payload) => {
+      if (payload && !state.leagues.length) {
         db.collection('userLeagues').doc(payload)
           .get()
           .then((leagues) => {
@@ -26,14 +34,40 @@ export default {
             if (leagues.exists) {
               theLeagues = Object.values(leagues.data())
             }
-            context.commit('SET_LEAGUES', theLeagues)
+            commit('SET_LEAGUES', theLeagues)
           })
-      } else {
-        context.commit('SET_LEAGUES', [])
+      }
+    },
+    fetchLeagueSchedule: ({ state, commit }, payload) => {
+      if (payload && !state.leagueSchedule.length) {
+        db.collection('leagueSchedule').doc(payload)
+          .get()
+          .then((leagueSchedule) => {
+            let theLeagueSchedule = []
+            if (leagueSchedule.exists) {
+              theLeagueSchedule = Object.values(leagueSchedule.data())
+            }
+            commit('SET_LEAGUE_SCHEDULE', theLeagueSchedule)
+          })
+      }
+    },
+    fetchLeagueUsers: ({ state, commit }, payload) => {
+      if (payload && !state.leagueUsers.length) {
+        db.collection('standardLeagueUsers').doc(payload)
+          .get()
+          .then((leagueUsers) => {
+            let theLeagueUsers = []
+            if (leagueUsers.exists) {
+              theLeagueUsers = Object.values(leagueUsers.data())
+            }
+            commit('SET_LEAGUE_USERS', theLeagueUsers)
+          })
       }
     }
   },
   getters: {
-    getUserLeagues: state => state.leagues
+    getUserLeagues: state => state.leagues,
+    getLeagueUsers: state => state.leagueUsers,
+    getLeagueSchedule: state => state.leagueSchedule
   }
 }
