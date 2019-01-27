5b64d50c18b8305e2f00f43598550e15c1ba26c4
Matt Locke <matt@highlevel.io>
2019-01-26 21:47:56 -0700

    Add trash talk to league page

--- src/views/Leagues.vue
+++ src/views/Leagues.vue
@@ -50,19 +50,23 @@
               h2 Start Draft
             .column.is-narrow
               confirm-button(:customClasses="{'is-primary': true,'is-small': true,'is-pulled-right':true}" buttonText="Start Draft" confirmText="Are You Sure?" @confirm-it="startDraft") Start Draft
-        section
-          h2 League Message
-            button.button.is-secondary.is-small.is-pulled-right(@click="editingMessage = !editingMessage" v-if="isOwner") {{ editingMessage ? 'cancel' : 'edit' }}
-          hr
-          .wrap(v-if="editingMessage")
-            b-field(label="League Message")
-              b-input(type="textarea" v-model="league.message" rows="10")
-            button.button.is-primary(@click="updateLeague") Save Message
-            hr
-          vue-markdown(v-if="leagueMessage" :source="leagueMessage")
-          .wrap(v-else)
-            img(src="https://firebasestorage.googleapis.com/v0/b/overwatch-pickem.appspot.com/o/images%2Fleagues%2Fwelcome-to-your-league.jpg?alt=media&token=bbf8225c-6bd0-4b1a-b5e0-d864a3047395")
-            p Click on the edit button above to customize your league landing page!  Inform members of the rules you have, the prizes you're giving away - whatever makes sense!
+        b-tabs(v-model="activeContentTab")
+          b-tab-item(label="League Message")
+            section
+              h2 League Message
+                button.button.is-secondary.is-small.is-pulled-right(@click="editingMessage = !editingMessage" v-if="isOwner") {{ editingMessage ? 'cancel' : 'edit' }}
+              hr
+              .wrap(v-if="editingMessage")
+                b-field(label="League Message")
+                  b-input(type="textarea" v-model="league.message" rows="10")
+                button.button.is-primary(@click="updateLeague") Save Message
+                hr
+              vue-markdown(v-if="leagueMessage" :source="leagueMessage")
+              .wrap(v-else)
+                img(src="https://firebasestorage.googleapis.com/v0/b/overwatch-pickem.appspot.com/o/images%2Fleagues%2Fwelcome-to-your-league.jpg?alt=media&token=bbf8225c-6bd0-4b1a-b5e0-d864a3047395")
+                p Click on the edit button above to customize your league landing page!  Inform members of the rules you have, the prizes you're giving away - whatever makes sense!
+          b-tab-item(label="Trash Talk")
+            trash-talk
         section(v-if="canJoinLeague && (userData.isAdmin || userData.isAlpha)")
           b-field(label="password" v-if="league.password")
             b-input(type="password" v-model="localPassword")
@@ -85,12 +89,14 @@ import vueMarkdown from 'vue-markdown'
 import LeagueService from '@/services/league'
 
 import LeagueSchedule from '@/views/leagues/LeagueSchedule'
+import TrashTalk from '@/views/draft/TrashTalk'
 
 export default {
   name: 'Leagues',
   components: {
     LeagueSchedule,
-    vueMarkdown
+    vueMarkdown,
+    TrashTalk
   },
   data () {
     return {
@@ -98,6 +104,7 @@ export default {
         message: '',
         image: ''
       },
+      activeContentTab: 0,
       localPassword: '',
       editingMessage: false,
       draftStatus: '',
