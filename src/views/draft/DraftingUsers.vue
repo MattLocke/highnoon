c6eb18fdc661eeb36339b60424a326bd0d561855
MattLocke <matt@dancingondynamite.com>
2019-01-31 18:58:30 -0700

    remove extra markup from drafted users

--- src/views/draft/DraftingUsers.vue
+++ src/views/draft/DraftingUsers.vue
@@ -4,8 +4,7 @@
       .column.is-one-fifth-desktop.is-half-mobile(v-for="(user, index) in users")
         h3.underlined(:class="{'orange': user.userId == users[draft.activeDrafter].userId && !draftComplete}") {{ user.displayName }}
           span.is-pulled-right(v-if="user.userId == users[draft.activeDrafter].userId && !draftComplete") {{ draft.direction == 'forward' ? '>' : '<' }}
-        ul
-          player-card(v-for="pick in getUserPicks(user.userId)" :key="pick.id" :player="pick" :showRemove="false" :primaryColor="getColor(pick)" :score="pick.stats.fantasyScore || 0" :hidePhoto="true") {{ pick.name }}
+        player-card(v-for="pick in getUserPicks(user.userId)" :key="pick.id" :player="pick" :showRemove="false" :primaryColor="getColor(pick)" :score="pick.stats.fantasyScore || 0" :hidePhoto="true") {{ pick.name }}
 </template>
 
 <script>
