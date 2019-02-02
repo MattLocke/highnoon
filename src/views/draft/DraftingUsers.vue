116104959d9bcfde99fef559bd5c0088a3d1008f
MattLocke <matt@dancingondynamite.com>
2019-02-02 01:06:53 -0700

    remove variable name collision in drafting users component

--- src/views/draft/DraftingUsers.vue
+++ src/views/draft/DraftingUsers.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   .drafting-users
     .columns.is-multiline.is-mobile
-      .column.is-one-fifth-desktop.is-half-mobile(v-for="(user, index) in users")
-        h3.underlined(:class="{'orange': user.userId == users[draft.activeDrafter].userId && !draftComplete}") {{ user.displayName }}
-          span.is-pulled-right(v-if="user.userId == users[draft.activeDrafter].userId && !draftComplete") {{ draft.direction == 'forward' ? '>' : '<' }}
-        player-card(v-for="pick in getUserPicks(user.userId)" :key="pick.id" :player="pick" :showRemove="false" :primaryColor="getColor(pick)" :score="pick.stats.fantasyScore || 0" :hidePhoto="true") {{ pick.name }}
+      .column.is-one-fifth-desktop.is-half-mobile(v-for="(theUser, index) in users")
+        h3.underlined(:class="{'orange': theUser.userId == users[draft.activeDrafter].userId && !draftComplete}") {{ theUser.displayName }}
+          span.is-pulled-right(v-if="theUser.userId == users[draft.activeDrafter].userId && !draftComplete") {{ draft.direction == 'forward' ? '>' : '<' }}
+        player-card(v-for="pick in getUserPicks(theUser.userId)" :key="pick.id" :player="pick" :showRemove="false" :primaryColor="getColor(pick)" :score="pick.stats.fantasyScore || 0" :hidePhoto="true") {{ pick.name }}
 </template>
 
 <script>
