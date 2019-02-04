e59ebbcb8f476ab25b0cc7fb609859a6ec71db69
MattLocke <matt@dancingondynamite.com>
2019-02-03 23:02:25 -0700

    scoring info added

---
+++ src/views/leagues/ScoringInfo.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .scoring-info
+    collapsible(title-text="Scoring Info")
+      p When building your team, remember that points are based off of eliminations and hitpoints exchanged.  A player that plays more will score better than a single-map player (like Pine) will.  If a player plays more than once in a week, they'll only get their best day, not both days.
+      hr
+      .left-bar-item
+        span .5 points per elimination
+      .left-bar-item
+        span 1 point per 1000 damage
+      .left-bar-item
+        span 1 point per 1000 healing
+</template>
