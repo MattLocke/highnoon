3eb720974768805f82a22e3df9ca5ae35c588246
Zachary E Sohovich <zacharyesohovich@gmail.com>
2019-02-17 14:12:15 -0800

    fix bug where menu wasn't closing

--- src/components/HamburgerMenu.vue
+++ src/components/HamburgerMenu.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .wrap.hamburger-menu
     eva-icon.has-pointer(name="more-horizotnal" fill="white" @click="isOpen = !isOpen")
-    .dropdown.is-up(:class="{'is-active': isOpen }")
+    .dropdown.is-up(:class="{'is-active': isOpen }" @click="isOpen = !isOpen")
       .dropdown-menu(role="menu")
         .dropdown-content
           a.dropdown-item(v-for="(menuItem, index) in menuItems" @click="menuItem.linkFn" :key="index") {{ menuItem.linkText }}
