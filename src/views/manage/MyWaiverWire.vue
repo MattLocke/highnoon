7f743ebb54a93d7eaf5ced68e528fe508d94f986
MattLocke <matt@dancingondynamite.com>
2019-02-18 01:46:07 -0700

    show pending waiver wires after requesting them.

--- src/views/manage/MyWaiverWire.vue
+++ src/views/manage/MyWaiverWire.vue
@@ -220,6 +220,7 @@ export default {
         .then(() => {
           this.myPlayer = {}
           this.selectedPlayer = {}
+          this.getWaivers()
           this.$toast.open({
             message: 'Successfully requested!',
             type: 'is-success',
